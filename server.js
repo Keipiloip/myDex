9f3316801e43474965f8114d560b5a74241468dc
Keipiloip <Keipiloip@gmail.com>
2026-05-29 10:53:54 +0200

    Add Pokémon add form and fix minor issues

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const db = mysql.createConnection({
  host: 'localhost', 
  user: 'root', 
  password: '', 
- database: 'myDex' 
+ database: 'myDexapp' 
 }); 
-app.post('/leggtil', (req, res) => { 
- const { Navn, dex_id, Level, Type, Type2 } = req.body; 
+app.post('/add', (req, res) => { 
+ const { navn, dex_id, level, type, type2 } = req.body; 
  db.query( 
- 'INSERT INTO brukere (navn, dex_id, level, type, type2) VALUES (?, ?, ?, ?, ?)', 
- [Navn, dex_id, Level, Type, Type2], 
+ 'INSERT INTO pokemon (navn, dex_id, level, type, type2) VALUES (?, ?, ?, ?, ?)', 
+ [navn, dex_id, level, type, type2], 
  (err) => { 
- if (err) return res.send('Feil i databasen: ' + err.message); 
- res.send('Data lagt til!'); 
+ if (err) return res.send('Error in database: ' + err.message); 
+ res.send('Data added!'); 
  } 
  ); 
 }); 
